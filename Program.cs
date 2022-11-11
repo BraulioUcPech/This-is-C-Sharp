5d965ac1dfd56ada6dc4bc33e5511cf547759716
Braulio_ <braulioalexson@gmail.com>
2022-11-11 17:50:57 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: braulio
+ * Date: 03/11/2022
+ * Time: 04:53 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace SoyYoDeNuevo
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("HOLA MUNDO!");
+
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
