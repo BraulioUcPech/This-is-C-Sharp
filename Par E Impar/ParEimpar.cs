04c2ec91f00a1613d4b2fcc322055ea8859e4cf4
Bra-Ülio <braulioalexson@gmail.com>
2022-11-14 20:21:12 -0600

    Add files via upload

---
+++ Par E Impar/ParEimpar.cs
@@ -0,0 +1,39 @@
+﻿}/*
+ * Created by SharpDevelop.
+ * User: Alumno
+ * Date: 14/11/2022
+ * Time: 10:32 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace CONSICONDBOLE
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			/*eSCRIBIR UN PROGRAMA QUE PIDA AL USUARIO UN NUMERO ENTERO Y MUESTRE POR PANTALL SI ES PAR O IMPAR*/
+			int num;
+			
+			
+			Console.WriteLine("Ingresa un numero");
+			num = int.Parse(Console.ReadLine());
+			
+			if(num% 2 == 0){
+				Console.WriteLine("Par");
+			}
+			else{
+				Console.WriteLine("Impar");
+			}
+			
+			
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
