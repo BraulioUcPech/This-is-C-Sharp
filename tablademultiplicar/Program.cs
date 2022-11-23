37768c36efa029e4c6f88bbe731f5436b833d50b
Bra-Ülio <braulioalexson@gmail.com>
2022-11-23 15:09:08 -0600

    Add files via upload

---
+++ tablademultiplicar/Program.cs
@@ -0,0 +1,28 @@
+﻿// tabla de multiplicar
+
+using System;
+
+namespace tabla_de_multiplicar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, cont, res;
+            num = 0;
+            cont = 0;
+            res = 0;
+
+            Console.WriteLine("Ingrese el numero que desea multiplicar");
+            num = int.Parse(Console.ReadLine());
+
+            for (cont = 0; cont <= 10; cont++)
+            {
+                res = num * cont;
+                Console.WriteLine(num + " x " + cont + " = " + res);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
