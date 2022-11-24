de69ef9476708685c776a766e8ff2351c0e9dae8
Bra-Ülio <braulioalexson@gmail.com>
2022-11-23 20:36:13 -0600

    Add files via upload

---
+++ 50calificacionesrepro/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Created by Bra-Ülio.
+ * User: braulio
+ * Date:
+ * Time:
+ *
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+
+/*Leer 50 calificaciones de un grupo de alumnos. Calcule y escriba el porcentaje de reprobados. Tomando en cuenta que la calificación mínima aprobatoria es de70*/
+
+
+using System;
+
+namespace SoyYoDeNuevo._50calificacionesrepro{
+    class Program{
+        public static void Main(string[] args){
+            int calificacion;
+            float reprobados = 0;
+            int aprobados = 0;
+            int total = 0;
+            float porcentaje;
+
+            for (int cont = 0; cont < 10; cont++){
+
+                Console.WriteLine("Introduce la calificacion " + cont);
+                calificacion = int.Parse(Console.ReadLine());
+
+                if (calificacion < 70){
+                    reprobados++;
+                }else{
+                    aprobados++;
+                }
+                total++;
+            }
+            porcentaje = reprobados / total * 100;
+            Console.WriteLine("El porcentaje de reprobados es de " + porcentaje + "%");
+
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ crearuncuadrado/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Created by Bra-Ülio.
+ * User: braulio
+ * Date:
+ * Time:
+ *
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+
+using System;
+
+
+namespace SoyYoDeNuevo.crearuncuadrado{
+    class Program{
+        public static void Main(string[] args){
+
+
+
+            int lado;
+
+            Console.WriteLine("Introduce el lado del cuadrado");
+            lado = int.Parse(Console.ReadLine());
+
+
+            for (int cont = 0; cont < lado; cont++){
+                for (int cont2 = 0; cont2 < lado; cont2++){
+                    if (cont == 0 || cont == lado - 1 || cont2 == 0 || cont2 == lado - 1){
+                        Console.Write("*");
+                    }else{
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
+
