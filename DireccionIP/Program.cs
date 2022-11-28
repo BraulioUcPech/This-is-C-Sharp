f8a748c623f9ec5622fab77415e6cd2620d2c243
Bra-Ülio <braulioalexson@gmail.com>
2022-11-28 17:29:28 -0600

    Update Program.cs

--- DireccionIP/Program.cs
+++ DireccionIP/Program.cs
@@ -22,37 +22,49 @@ namespace DireccionIP{
         static void Main(string[] args){
 
 
-
-            Console.WriteLine("Ingrese la direccion IP del equipo");
+            Console.WriteLine("Ingrese la direccion IP de la maquina a reiniciar o apagar");
             string ip = Console.ReadLine();
 
-            Console.WriteLine("Ingrese 1 para reiniciar o 2 para apagar");
-            string opcion = Console.ReadLine();
+            Console.WriteLine("Ingrese la accion a realizar: 1 para reiniciar, 2 para apagar");
+            string accion = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el tiempo de espera");
+            int tiempo = Convert.ToInt32(Console.ReadLine());
 
 
-            if (opcion == "1"){
+            if (accion == "1"){
                 //reiniciar
-                string comando = "shutdown -m 1.1.0.0 -r -f -t 60 ";
-                ProcessStartInfo psi = new ProcessStartInfo("cmd.exe", "/c " + comando);
-                psi.RedirectStandardOutput = true;
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.FileName = "cmd.exe";
+                psi.Arguments = "/c shutdown -r -m \\\\" + ip + " -t " + tiempo;
                 psi.UseShellExecute = false;
+                psi.RedirectStandardOutput = true;
+                psi.RedirectStandardError = true;
                 psi.CreateNoWindow = true;
                 Process p = Process.Start(psi);
                 p.WaitForExit();
+                Console.WriteLine(p.StandardOutput.ReadToEnd());
+                Console.WriteLine(p.StandardError.ReadToEnd());
             }
-            else if (opcion == "2"){
+            else if (accion == "apagar"){
                 //apagar
-                string comando = "shutdown -m 192.168.1.12 -s -f -t 60";
-                ProcessStartInfo psi = new ProcessStartInfo("cmd.exe", "/c " + comando);
-                psi.RedirectStandardOutput = true;
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.FileName = "cmd.exe";
+                psi.Arguments = "/c shutdown -s -m \\\\" + ip + " -t " + tiempo;
                 psi.UseShellExecute = false;
+                psi.RedirectStandardOutput = true;
+                psi.RedirectStandardError = true;
                 psi.CreateNoWindow = true;
                 Process p = Process.Start(psi);
                 p.WaitForExit();
+                Console.WriteLine(p.StandardOutput.ReadToEnd());
+                Console.WriteLine(p.StandardError.ReadToEnd());
             }
             else{
-                Console.WriteLine("Opcion no valida");
+                Console.WriteLine("No se reconoce la accion");
             }
+            
+            //el codigo fue modificado, para mojorarlo.
         }
     }
 }
